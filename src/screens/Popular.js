b66c449bd8ade7266a1011930de10c743e55b21e
KevinDev115 <galindok829@gmail.com>
2021-01-23 17:14:02 -0500

    Movie populars

--- src/screens/Popular.js
+++ src/screens/Popular.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 
-export default function Popular() {
+import {getPopularMoviesApi} from '../api/movies';
+
+export default function Popular(props) {
+  const {navigation} = props;
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    getPopularMoviesApi(1).then((response) => {
+      setMovies(response.results);
+    });
+  }, []);
+
   return (
     <View>
       <Text>Popular</Text>
